4ffa39c359fe655426bdbb2edfff0c075331b0cf
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-04 18:31:48 +0530

    Added :Print the year is a Leap Year or not.

---
+++ src/com/bridgelabz/LeapYearOrNot.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year :");
+        int year= sc.nextInt();
+        if((year%400==0) || (year%4==0 && year%100!=0)) {
+            System.out.println("It is leap year");
+        }
+        else
+            System.out.println("It is not leap year");
+    }
+}
