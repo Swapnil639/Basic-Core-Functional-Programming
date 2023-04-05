3d65dfea99fa22028bc0566f07d7d84a6220f9f5
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-05 23:08:37 +0530

    Added : Scanner scanner = new Scanner(System.in);

---
+++ src/com/bridgelabz/Distance.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number :");
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        double distance = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        System.out.println("Euclidean distance :"+ distance);
+    }
+}
